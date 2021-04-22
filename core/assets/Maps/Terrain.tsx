74d5a749ca01a3a6d0d4550bc6eb6869368799e6
Zenovore <agustinus.alexander17@gmail.com>
2021-04-22 15:39:29 +0700

    Add map.tmx fixed change map loader path

---
+++ core/assets/Maps/Terrain.tsx
@@ -0,0 +1,54 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="TEST5" tilewidth="32" tileheight="32" tilecount="192" columns="12">
+ <image source="Terrain.png" width="384" height="512"/>
+ <tile id="12">
+  <properties>
+   <property name="Biome" value="Grassland"/>
+  </properties>
+ </tile>
+ <tile id="13">
+  <properties>
+   <property name="Biome" value="Grassland"/>
+  </properties>
+ </tile>
+ <tile id="16">
+  <properties>
+   <property name="Biome" value="Grassland"/>
+  </properties>
+ </tile>
+ <tile id="17">
+  <properties>
+   <property name="Biome" value="Grassland"/>
+  </properties>
+ </tile>
+ <tile id="134">
+  <properties>
+   <property name="Biome" value="Water"/>
+  </properties>
+ </tile>
+ <tile id="135">
+  <properties>
+   <property name="Biome" value="Water"/>
+  </properties>
+ </tile>
+ <tile id="136">
+  <properties>
+   <property name="Biome" value="Water"/>
+  </properties>
+ </tile>
+ <tile id="137">
+  <properties>
+   <property name="Biome" value="Water"/>
+  </properties>
+ </tile>
+ <tile id="138">
+  <properties>
+   <property name="Biome" value="Water"/>
+  </properties>
+ </tile>
+ <tile id="139">
+  <properties>
+   <property name="Biome" value="Water"/>
+  </properties>
+ </tile>
+</tileset>
