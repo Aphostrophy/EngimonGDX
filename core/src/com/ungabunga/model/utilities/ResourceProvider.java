cd2dc34bfe59cf56d759301796a67fbefd07e7f7
Jesson Yo <Aphostrophy@gmail.com>
2021-04-23 22:30:09 +0700

    player ketengah, nambahin placeholder untuk randomize engimon

--- core/src/com/ungabunga/model/utilities/ResourceProvider.java
+++ core/src/com/ungabunga/model/utilities/ResourceProvider.java
@@ -2,7 +2,11 @@ package com.ungabunga.model.utilities;
 
 import com.ungabunga.model.entities.Engimon;
 import com.ungabunga.model.entities.Skill;
+import com.ungabunga.model.enums.CellType;
 
+import java.util.concurrent.ThreadLocalRandom;
+
+import java.util.ArrayList;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 public class ResourceProvider {
@@ -18,6 +22,16 @@ public class ResourceProvider {
         for(Skill skill : this.skills){
             if(skill.getSkillName().equals(name)){
                 return skill;
+            }
+        }
+        return null;
+    }
+
+    public Engimon randomizeEngimon(CellType biome){
+        ArrayList<Engimon> candidates = new ArrayList<>();
+        for(Engimon engimon: this.engimon){
+            if(engimon.getElements().contains(biome)){
+                candidates.add(engimon);
             }
         }
         return null;
