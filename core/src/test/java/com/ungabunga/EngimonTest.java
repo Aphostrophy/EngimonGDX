c954932025a74752b47aa2a65c9b16596e8a7dfc
roysimbolon <71599375+roysimbolon@users.noreply.github.com>
2021-04-24 18:00:16 +0700

    Update EngimonTest.java

--- core/src/test/java/com/ungabunga/EngimonTest.java
+++ core/src/test/java/com/ungabunga/EngimonTest.java
@@ -1,13 +1,13 @@
 package com.ungabunga;
 
-import java.util.*;
-
-import com.ungabunga.model.entities.Skill;
+import com.ungabunga.model.entities.*;
 import com.ungabunga.model.enums.IElements;
-import com.ungabunga.model.entities.Engimon;
-import com.ungabunga.model.utilities.Pair;
+import javafx.util.Pair;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class EngimonTest {
     @Test
@@ -200,4 +200,60 @@ public class EngimonTest {
         Assertions.assertEquals(6, engimon.getSkills().get(0).getBasePower());
         Assertions.assertEquals(1, engimon.getSkills().get(0).getMasteryLevel());
     }
+    @Test
+    public void testGetHitungPower(){
+        List<IElements> engimonElements = new ArrayList<>();
+        List<IElements> skillElements = new ArrayList<>();
+        List<Skill> skills = new ArrayList<>();
+
+        Pair<String, String> parentName = new Pair("Parent A","Parent B");
+        Pair<String, String> parentSpecies = new Pair("Species A","Species B");
+
+        engimonElements.add(IElements.ELECTRIC);
+        skillElements.add(IElements.ELECTRIC);
+
+        Skill skill = new Skill("Capacitor", skillElements, 6, 1);
+        skills.add(skill);
+
+        Engimon engimon = new Engimon("NamaPokemon", "Jolteon", "Keep the energy", 1, engimonElements, skills, parentName, parentSpecies);
+        Assertions.assertEquals(1, engimon.getLevel());
+        Assertions.assertEquals(6, engimon.getSkills().get(0).getBasePower());
+        Assertions.assertEquals(1, engimon.getSkills().get(0).getMasteryLevel());
+
+    }
+    @Test
+    public void testGetPosition(Object AnimationSet){
+        Player player = new Player("NamaPlayer",AnimationSet,1,1);
+        Assertions.assertEquals(1, player.getPosition().getFirst());
+        Assertions.assertEquals(1, player.getPosition().getSecond());
+
+
+    }
+    @Test
+    public void testGetCapacity(){
+        List<Integer> capacity = new ArrayList<>();
+        capacity.add(10);
+        Inventory inventory = new Inventory();
+        Assertions.assertEquals(10, inventory.getMaxCapacity());
+    }
+    @Test
+    public void testGetFileSlot(){
+        List<Integer> neff = new ArrayList<>();
+        neff.add(10);
+        Inventory inventory = new Inventory();
+        Assertions.assertEquals(10, inventory.getFilledSlot());
+    }
+    @Test
+    public void testGetAmount(){
+        List<Integer> amount = new ArrayList<>();
+        amount.add(1);
+        SkillItem skillItem = new SkillItem("NamaSkill",10);
+        Assertions.assertEquals(10, skillItem.getAmount());
+    }
+    @Test
+    public void testGetSkillName(){
+        SkillItem skillItem = new SkillItem("NamaSkill",10);
+        Assertions.assertEquals("NamaSkill", skillItem.getName());
+    }
+
 }
